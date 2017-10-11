c090c06ad383329eb6fc208203a66eea2bc41592
Tolulope.Awode <Tolulope.Awode@WEMA-WDB-LC0370>
2017-10-11 02:12:11 +0100

    Add project files.

---
+++ Core.Payment.DAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Payment.DAL
+{
+    public class Class1
+    {
+    }
+}
